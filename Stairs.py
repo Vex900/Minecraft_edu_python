015f2ae3fadb993d5d8299fd326e5a6a7486ec14
Vex900 <martincek.botev@gmail.com>
2025-12-04 17:34:00 +0100

    Update Stairs.py

--- Stairs.py
+++ Stairs.py
@@ -8,3 +8,17 @@ def buildstairs(height, lenght):
     agent.move(DOWN, 1)
     agent.move(BACK, 1)
     agent.set_assist(PLACE_ON_MOVE, True)
+    agent.set_slot(2)
+    agent.move(FORWARD, lenght)
+    agent.set_assist(PLACE_ON_MOVE, False)
+    agent.turn(TurnDirection.LEFT)
+    agent.turn(TurnDirection.LEFT)
+    agent.move(BACK, 1)
+    agent.move(UP, 1)
+    agent.set_slot(1)
+    for index in range(height):
+        agent.move(DOWN, 1)
+        agent.place(FORWARD)
+        agent.move(BACK, 1)
+
+player.on_chat("buildstairs2", buildstairs)
